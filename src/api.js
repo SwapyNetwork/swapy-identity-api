d0ba7ecb262b4317398af7f943462be8ccd443d6
Luis Philipe <luizsale10@gmail.com>
2018-01-30 19:19:37 -0200

    handle web3 (incomplete)

--- src/api.js
+++ src/api.js
@@ -1,10 +1,59 @@
+import Web3 from 'web3'
+import { 
+    setProvider,
+    initTree,
+    saveTree,
+    saveData,
+    getTree,
+    getData,
+    dfs,
+    insertNodes,
+    updateNode,
+    removeNode } from './ipfs'
+
+const networks = {
+    'ropsten': { id: '0x3', protocol: '' },
+    'rinkeby': { id: '0x4', protocol: '' },
+}
+
+const DEFAULTNETWORK = 'ropsten'
+
+let web3
+let network
+//@todo contracts ABI
+let IdentityProtocol
+let IdentityProtocolContract
+let Identity
+let IdentityContract
+let MultiSigIdentity
+let MultiSigIdentityContract
+
+
+const initAPI = (privateKey, httpProvider, _networkName = DEFAULTNETWORK)  => {
+    web3 = new Web3(httpProvider) 
+    network = networks[_networkName]
+    IdentityProtocolContract = new web3.eth.Contract(IdentityProtocolABI.abi)
+    IdentityProtocolContract.options.address = network.protocol
+    const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+    web3.eth.accounts.wallet.add(account)
+    web3.eth.defaultAccount = account.address;   
+}
+
 /**
   * Instantiates a new personal identity.
   *
   * @param       {String}                 profileData  the profile data location on IPFS
   * @return      {Promise<Object, Error>}              A promise that resolves with the transaction object or rejects with an error                             
   */
-const createPersonalIdentity = (profileData) => {}
+const createPersonalIdentity = (profileData) => {
+    return IdentityProtocolContract.methods
+    .createPersonalIdentity(profileData)
+    .send({
+        gas: 150000,
+        gasPrice: web3.utils.toWei(this.gasPrice, 'gwei')
+    })
+}
+
 /**
   * Instantiates a new multi signature identity.
   *
@@ -13,25 +62,41 @@ const createPersonalIdentity = (profileData) => {}
   * @param   {Integer}                 required     the required number of signatures 
   * @return  {Promise<Object, Error>}               A promise that resolves with the transaction object or rejects with an error                          
   */
-const createMultiSigIdentity = (profileData, owners, ) => {}
+const createMultiSigIdentity = (profileData, owners, required) => {
+    return IdentityProtocolContract.methods
+    .createMultiSigIdentity(profileData, owners, required)
+    .send({
+        gas: 150000,
+        gasPrice: web3.utils.toWei(this.gasPrice, 'gwei')
+    })
+}
 
 /**
   * Creates a new transaction.
   *
-  * @param   {String}                  destination  destination address 
-  * @param   {Integer}                 value        tx value
-  * @param   {String}                  data         tx data  
-  * @return  {Promise<Object, Error>}               A promise that resolves with the transaction object or rejects with an error                          
+  * @param   {String}                 destination  destination address 
+  * @param   {Integer}                value        tx value
+  * @param   {String}                 data         tx data  
+  * @return  {Promise<Object, Error>}              A promise that resolves with the transaction object or rejects with an error                          
   */
 const sendTransaction = (destination, value, data) => {}
 
 /**
   * Signs a multi sig transaction.
   *
-  * @param   {Integer}                 transactionId transaction's index 
-  * @return  {Promise<Object, Error>}                A promise that resolves with the transaction object or rejects with an error                          
+  * @param   {String}                 identityAddress  transaction's index 
+  * @param   {Integer}                transactionId    transaction's index 
+  * @return  {Promise<Object, Error>}                  A promise that resolves with the transaction object or rejects with an error                          
   */
-const signTransaction = (transactionId) => {}
+const signTransaction = (identityAddress, transactionId) => {
+    MultiSigIdentityContract.options.address = identityAddress
+    return MultiSigIdentityContract.methods
+    .signTransaction(transactionId)
+    .send({
+        gas: 150000,
+        gasPrice: web3.utils.toWei(this.gasPrice, 'gwei')
+    })
+}
 
 /**
   * Set a new profile data
@@ -48,7 +113,6 @@ const changeProfileData = (profileData) => {}
   * @return  {Promise<Object, Error>}           A promise that resolves with the transaction object or rejects with an error                          
   */
 const addOwner = () => {}
-
 
 /**
   * Removes a multi sig owner
