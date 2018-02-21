cbdd40077fa212efcb56eac67e1d42987428dee4
Luis Philipe <luizsale10@gmail.com>
2018-02-21 20:21:00 -0300

    publish on npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,44 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'lib'),
     filename: 'bundle.js'   
 	},
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
-      }
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+        }]
+    },
+    node: {
+        console: false,
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    },
+    resolve: {
+        modules: [ './src', 'node_modules' ],
+        extensions: ['.js', '.json']
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'lib')
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          sourceMap: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+              'NODE_ENV': JSON.stringify('production')
+            }
+        }),
     ]
-  },
-  node: {
-    console: false,
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  },
-  resolve: {
-    modules: [ './src', 'node_modules' ],
-    extensions: ['.js', '.json']
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
 };
