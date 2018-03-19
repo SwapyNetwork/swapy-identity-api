fca8b04e39813887dc674e89063ab3a65022b659
Luis Philipe <luizsale10@gmail.com>
2018-03-19 16:28:07 -0300

    update protocol address on testnets

--- src/config/ethereum.js
+++ src/config/ethereum.js
@@ -1,6 +1,6 @@
 const ethAddresses = {
-    'ropsten': { id: '*', protocol: '0x37c3284b2d99f1c805092ecec7e0658638377394', token: '0xddcc1ebf2f4d47b485a201b64f41c1ddd18ab247' },
-    'rinkeby': { id: '*', protocol: '0x6b592dd3af172e2a2f20819f9db6e52205533233', token: '0xc32d87cbc83983faf37cc947584c115ec6b4e197' },
+    'ropsten': { id: '*', protocol: '0x2e5ad42192284e5b2e8b4d7455b3662f4211136c', token: '0xddcc1ebf2f4d47b485a201b64f41c1ddd18ab247' },
+    'rinkeby': { id: '*', protocol: '0xc8d8e87dfbb0e5ad12da35c0f37133ea8c71d01b', token: '0xc32d87cbc83983faf37cc947584c115ec6b4e197' },
     'ganache': { id: '*', protocol: '0x7a3fac7330ebf80317a0dc25ee592c5bca1e8457', token: '0x688389535167602ddbca611e2bde323963bfb2da' },
 }
 
