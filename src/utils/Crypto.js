5f79abff3eb2d8abb68fcb0362439cedfa3087c3
Luis Philipe <luizsale10@gmail.com>
2018-03-28 17:25:33 -0300

    AES cipher

--- src/utils/Crypto.js
+++ src/utils/Crypto.js
@@ -4,14 +4,30 @@ import * as crypto from 'crypto-browserify'
 
 class Crypto {
     
-    static async encrypt(key, content) {
+    static async encryptEcc(key, content) {
         const encrypted = await EthCrypto.encryptWithPublicKey(key, content)
         return encrypted
     }
 
-    static async decrypt(key, encrypted) {
+    static async decryptEcc(key, encrypted) {
         const content = await EthCrypto.decryptWithPrivateKey(key, encrypted)
         return content
+    }
+
+    static async encrypt(key, content) {
+        const suite = 'aes-128-ctr'
+        const cipher = crypto.createCipher(suite,key)
+        let crypted = cipher.update(content,'utf8','hex')
+        crypted += cipher.final('hex');
+        return crypted
+    }
+
+    static async decrypt(key, encrypted) {
+        const suite = 'aes-128-ctr'
+        const decipher = crypto.createDecipher(suite,key)
+        let dec = decipher.update(encrypted,'hex','utf8')
+        dec += decipher.final('utf8');
+        return dec
     }
 
     static randomBytes(size) { return crypto.randomBytes(size) }
